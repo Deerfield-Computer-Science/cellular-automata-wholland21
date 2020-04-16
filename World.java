ca0a8826b64bc3ddb20b427d04842558ae47ae31
Will Holland <wholland21@deerfield.edu>
2020-04-16 14:45:36 -0400

    4/16

--- World.java
+++ World.java
@@ -18,7 +18,7 @@ public class World {
 
 	public void letTimePass(){
 
-		checkAround2();
+		checkAround3();
 		moveCreatures();
 		creaturesGetOlder();
 		purgeTheDead();		
@@ -27,78 +27,102 @@ public class World {
 
 	public void moveCreatures() {
 		for(int i=0; i< creatureList.size(); i++) {
+		//	if (creatureList.get(i).getMyColor()== Color.GREEN)
 			creatureList.get(i).move(i);
 		}
 	}
 
-
+/*
 	public void checkAround() {
 		for(int index=0; index<creatureList.size(); index++) {
 			int x=creatureList.get(index).getMyLocation().getX();			
 			int y=creatureList.get(index).getMyLocation().getY();
-			int level=creatureList.get(index).getLevel();
+			int levelOfOGCreature=creatureList.get(index).getLevel();
 
 			for (int plus=index+1; plus<creatureList.size()-1;plus++) {	
 
 				if( (creatureList.get(plus).getMyLocation().getX())+1==x 
 						&& (creatureList.get(plus).getMyLocation().getY())+1==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())+1==x 
 						&& (creatureList.get(plus).getMyLocation().getY())-1==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())+1==x 
 						&& (creatureList.get(plus).getMyLocation().getY())==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())-1==x 
 						&& (creatureList.get(plus).getMyLocation().getY())+1==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())-1==x 
 						&& (creatureList.get(plus).getMyLocation().getY())-1==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())-1==x 
 						&& (creatureList.get(plus).getMyLocation().getY())==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())==x 
 						&& (creatureList.get(plus).getMyLocation().getY())-1==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 				if( (creatureList.get(plus).getMyLocation().getX())==x 
 						&& (creatureList.get(plus).getMyLocation().getY())+1==y ) {
-					spreadDisease(level, plus);
+					spreadDisease(levelOfOGCreature, plus);
 				}
 
 			}
 
 		}
 	}
-
+*/
 	public void checkAround2() {
 		boolean x2 = false;
 		boolean y2 = false;
 		for(int index=0; index<creatureList.size(); index++) {
 			int x=creatureList.get(index).getMyLocation().getX();             
 			int y=creatureList.get(index).getMyLocation().getY();
-			int level=creatureList.get(index).getLevel();
-			for (int plus=index+1; plus<creatureList.size();plus++) {
-				if(creatureList.get(plus).getMyLocation().getX()==x && creatureList.get(plus).getMyLocation().getY()==y) {
-
+			int levelOfOGCreature=creatureList.get(index).getLevel();
+			for (int nextCreature=index+1; nextCreature<creatureList.size();nextCreature++) {
+				if(creatureList.get(nextCreature).getMyLocation().getX()==x 
+						&& creatureList.get(nextCreature).getMyLocation().getY()==y) {
 				} else {
 					for (int xVal = -1 ; xVal <= 1 ;xVal++) {     
 						for(int yVal = -1 ; yVal <= 1; yVal++) {
-							if(creatureList.get(plus).getMyLocation().getX()==x+xVal) {
+							if(creatureList.get(nextCreature).getMyLocation().getX()==x+xVal) {
 								x2=true;
 							}
-							if(creatureList.get(plus).getMyLocation().getY()==y+yVal) {
+							if(creatureList.get(nextCreature).getMyLocation().getY()==y+yVal) {
 								y2=true;
 							}
 							if (x2 == true && y2 == true) {
-								spreadDisease(level, plus);
+								spreadDisease(levelOfOGCreature, nextCreature, index);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	public void checkAround3() {
+		
+		for(int index=0; index<creatureList.size(); index++) {
+			int x=creatureList.get(index).getMyLocation().getX();             
+			int y=creatureList.get(index).getMyLocation().getY();
+			int levelOfOGCreature=creatureList.get(index).getLevel();
+			for (int nextCreature=0; nextCreature<creatureList.size();nextCreature++) {
+				if(creatureList.get(nextCreature).getMyLocation().getX()==x 
+						&& creatureList.get(nextCreature).getMyLocation().getY()==y) {
+				} else {
+					for (int xVal = -1 ; xVal <= 1 ;xVal++) {     
+						for(int yVal = -1 ; yVal <= 1; yVal++) {
+							if(creatureList.get(nextCreature).getMyLocation().getX()==x+xVal && 
+									creatureList.get(nextCreature).getMyLocation().getY()==y+yVal 
+									&& creatureList.get(index).getLevel() != 0 ) {
+								spreadDisease(levelOfOGCreature, nextCreature, index);
 							}
 						}
 					}
@@ -107,27 +131,36 @@ public class World {
 		}
 	}
 
-	public void spreadDisease(int level,  int plus) {
-		if(creatureList.get(plus).getLevel()<level) {
-			int x=creatureList.get(plus).getMyLocation().getX();
-			int y=creatureList.get(plus).getMyLocation().getY();
-			int lifeSpan= creatureList.get(plus).getMyLifeSpan();
-			creatureList.add(new aysmptomatic(lifeSpan,new Location(x,y), Color.BLUE, WorldController.theWorld, level));
+	public void spreadDisease(int levelOfOGCreature,  int nextCreature, int index) {
+		System.out.println("in Spread Disease");
+		System.out.println(creatureList.get(nextCreature).getClass());
+		System.out.println(creatureList.get(index).getClass());
+		if(creatureList.get(nextCreature).getLevel()<levelOfOGCreature && creatureList.get(nextCreature).getLevel()==0) {
+			int x=creatureList.get(nextCreature).getMyLocation().getX();
+			int y=creatureList.get(nextCreature).getMyLocation().getY();
+			int lifeSpan= creatureList.get(nextCreature).getMyLifeSpan();
+			creatureList.add(new aysmptomatic(lifeSpan,new Location(x,y), Color.BLUE, this, levelOfOGCreature));
+			System.out.println("added another asymp");
 			
+		//	creatureList.get(plus).isDead();
 			
-			int lifeSpan2=creatureList.get(plus).getMyLifeSpan();
-			creatureList.get(plus).setAge(lifeSpan2);
+
+			int lifeSpan2=creatureList.get(nextCreature).getMyLifeSpan();
+			creatureList.get(nextCreature).setAge(lifeSpan2);
 		}
 		purgeTheDead();
 	}
 
 	public void purgeTheDead(){
 		int i=0;
+		System.out.println("in purge the dead");
 		while(i<creatureList.size()){
-			if(creatureList.get(i).isDead())
+			if(creatureList.get(i).isDead()) {
 				creatureList.remove(i);
-			else
+				System.out.println("purging");
+			}else {
 				i++;
+			}
 		}	
 	}
 
--- WorldController.java
+++ WorldController.java
@@ -5,7 +5,7 @@ import acm.util.*;
 
 public class WorldController extends GraphicsProgram {
 	
-	protected static World theWorld;
+	private World theWorld;
 	private GCanvas theWorldCanvas;
 	public static final int APPLICATION_WIDTH = 1000;
 	public static final int APPLICATION_HEIGHT = 1000;
@@ -13,9 +13,7 @@ public class WorldController extends GraphicsProgram {
 	public void run(){	
 		setUpWorld();
 		runWorld();
-		
-		System.out.print(theWorld.getCreatureList().get(0));
-	
+		countCreature();
 	}
 	
 	public void init(){
@@ -26,20 +24,50 @@ public class WorldController extends GraphicsProgram {
 		theWorld = new World(100,100);
 		
 		theWorld.getCreatureList().add(new healthy(52,new Location(10,10), Color.GREEN, theWorld, 0));
-		theWorld.getCreatureList().add(new healthy(52,new Location(10,11), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(10,11), Color.BLUE, theWorld, 1));
 		theWorld.getCreatureList().add(new healthy(52,new Location(11,12), Color.GREEN, theWorld, 0));
 		theWorld.getCreatureList().add(new healthy(52,new Location(11,13), Color.GREEN, theWorld, 0));
-		theWorld.getCreatureList().add(new healthy(52,new Location(12,14), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new healthy(52,new Location(12,14), Color.GREEN, theWorld, 0));
 		theWorld.getCreatureList().add(new healthy(52,new Location(12,15), Color.GREEN, theWorld, 0));
 		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(11,15), Color.BLUE, theWorld, 1));
 
+		theWorld.getCreatureList().add(new healthy(52,new Location(14,16), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(10,16), Color.BLUE, theWorld, 1));
+		theWorld.getCreatureList().add(new healthy(52,new Location(15,16), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(16,17), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new healthy(52,new Location(12,17), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(17,18), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(18,18), Color.BLUE, theWorld, 1));
+		
+		theWorld.getCreatureList().add(new healthy(52,new Location(19,19), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(10,11), Color.BLUE, theWorld, 1));
+		theWorld.getCreatureList().add(new healthy(52,new Location(20,19), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(22,20), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(24,20), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(26,20), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(11,23), Color.BLUE, theWorld, 1));
+
+		theWorld.getCreatureList().add(new healthy(52,new Location(28,23), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(10,11), Color.BLUE, theWorld, 1));
+		theWorld.getCreatureList().add(new healthy(52,new Location(30,25), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(32,25), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(35,23), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(37,30), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(11,30), Color.BLUE, theWorld, 1));
+		
+		theWorld.getCreatureList().add(new healthy(52,new Location(50,50), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(90,91), Color.BLUE, theWorld, 1));
+		theWorld.getCreatureList().add(new healthy(52,new Location(51,52), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(51,53), Color.GREEN, theWorld, 0));		theWorld.getCreatureList().add(new healthy(52,new Location(52,54), Color.GREEN, theWorld, 0));
+		theWorld.getCreatureList().add(new healthy(52,new Location(52,55), Color.GREEN, theWorld, 0));
+//		theWorld.getCreatureList().add(new aysmptomatic(52,new Location(51,55), Color.BLUE, theWorld, 1));
 		
 		theWorldCanvas = this.getGCanvas();
 	}
 	
 	public void runWorld(){
 		drawWorld();
-		for(int i=0; i<25;i++){
+		for(int i=0; i<52;i++){
 			theWorld.letTimePass();
 			pause(500);
 			drawWorld();
@@ -68,5 +96,20 @@ public class WorldController extends GraphicsProgram {
 			r.setFilled(true);
 			theWorldCanvas.add(r);
 		}
+	}
+	public void countCreature() {
+		int aypm=0;
+		int healthy=0;
+		for(LifeForm x: theWorld.getCreatureList()) {
+			if(x.getMyColor()==Color.BLUE) {
+				aypm= aypm+1;
+			}
+			if(x.getMyColor()==Color.GREEN) {
+				healthy= healthy+1;
+			}
+		}
+		System.out.print("Creature list size:"+ theWorld.getCreatureList().size());
+		System.out.print("number of asymp:"+ aypm);
+		System.out.print("number of healthy:"+ healthy+ " ");
 	}
 }
